ac3befad7dfc071184180891133e8fa59959a0cb
Edison Moya <e.j.moya.arias@gmail.com>
2021-03-17 09:28:46 -0500

    Selector styles

--- src/components/MetricSelector.js
+++ src/components/MetricSelector.js
@@ -10,30 +10,29 @@ function MetricSelector() {
     const caret = (<FontAwesomeIcon icon={faCaretDown} />);
     let stateStyle = { height: 35, visibility: 'visible' }
 
-    const openSelector = (e) => {
-        // const currentTop = e.target.offsetTop
+    const openSelector = () => {
         const selector = document.getElementById('field-selector-values-state-fs');
+        var mItems = document.getElementsByClassName('field-state-fs');
         if (openMS) {
             if (selector.classList.contains('open')) {
                 selector.classList.remove("open");
+                Array.prototype.forEach.call(mItems, item => item.classList.remove("hidden"));
+                selector.style.paddingLeft = '15px';
+                selector.style.paddingRight = '21px';
             } else {
                 selector.classList.add("open");
+                Array.prototype.forEach.call(mItems, item => item.classList.remove("hidden"));
+                selector.style.paddingLeft = '15px';
+                selector.style.paddingRight = '15px';
             }
         } else {
             setOpenMS(true);
         }
-        
-        //const top = currentTop + 30 + 'px'
-
-        //list.style.width = '620px'
-        //list.style.left = '900px'
-        //list.style.top = top
-        //list.classList.add('open')
     }
 
     useEffect(() => {
-        const es = window.cf.getProviderByConfig({ name: 'Elasticsearch'});
 
+        const es = window.cf.getProviderByConfig({ name: 'Elasticsearch'});
         stateMetrics()
             .element('state-fs')
             .on('field-selected', e => { 
@@ -49,11 +48,11 @@ function MetricSelector() {
     return (
         <div className={`metric-selector`} id="metric-selector">
             <div className="metric-title"></div>
-            <div className="selected-metric" id="metric-animation">
+            <div className="selected-metric" id="metric-animation" onClick={openSelector}>
                 <a>{field}</a>
                 <a>{caret}</a>
             </div>
-            <div className="selector" id="state-fs" style={stateStyle} onClick={openSelector}></div>
+            <div className="selector" id="state-fs" style={stateStyle}></div>
         </div>
     )
 }
